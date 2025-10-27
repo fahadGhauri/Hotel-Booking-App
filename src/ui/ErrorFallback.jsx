f1873fa81bbf2820a09c59e8b1108ee12f8a81e0
fa03044 <fa03044882712@gmail.com>
2025-10-27 07:20:23 -0700

    Change

--- src/ui/ErrorFallback.jsx
+++ src/ui/ErrorFallback.jsx
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 import Heading from "./Heading";
-import GlobalStyle from "../Styles/GlobalStyle";
+import GlobalStyle from "./styles/GlobalStyle";
 import Button from "./Button";
 
 const StyledErrorFallback = styled.main`
